76f56c4163ff997bd26525f576e0030b26383b28
戴文辉 <380169786@qq.com>
2025-08-20 23:24:45 +0800

    修改配置

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,16 +1,16 @@
 import type { HttpContentType } from "./http/config";
 
-export const HOME_PATH = import.meta.env.VITE_APP_HOME_PATH;
+export const HOME_PATH = import.meta.env.VITE_APP_HOME_PATH || "";
 
-export const PUBLIC_PATH = import.meta.env.VITE_PUBLIC_PATH;
+export const PUBLIC_PATH = import.meta.env.VITE_PUBLIC_PATH || "";
 /** 项目名称 */
-export const APP_TITLE = import.meta.env.VITE_APP_TITLE;
+export const APP_TITLE = import.meta.env.VITE_APP_TITLE || "";
 /** 页面底部文案 */
-export const APP_FOOTER_CONTENT = import.meta.env.VITE_APP_FOOTER_CONTENT;
+export const APP_FOOTER_CONTENT = import.meta.env.VITE_APP_FOOTER_CONTENT || "";
 /** 请求成功编码 */
-export const HTTP_SUCCESS_CODE = import.meta.env.VITE_HTTP_SUCCESS_CODE;
+export const HTTP_SUCCESS_CODE = import.meta.env.VITE_HTTP_SUCCESS_CODE || "";
 /** 接口前缀 */
-export const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH;
+export const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH || "";
 /**
  * 请求超时时间
  */
